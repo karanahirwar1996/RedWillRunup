2278d404027a4f24787ad185942bce0fbd12c9d6
Karan Ahirwar <114184956+karanahirwar1996@users.noreply.github.com>
2023-06-04 16:27:59 +0530

    Update RedWillRunup.py

--- RedWillRunup.py
+++ RedWillRunup.py
@@ -31,10 +31,10 @@ filtered_df = analysis_df.loc[(analysis_df['mnChange'] > 0) &
                               (analysis_df['Match Stock'] != "")].copy()
 
 # Sort filtered data based on "3mAvgVol" and "vol" in descending order
-sorted_df = filtered_df.sort_values(by=["Buying-Triger","Deep Score", "vol"], ascending=[True,False,False]).reset_index(drop=True)
+sorted_df = filtered_df.sort_values(by=["Diff","Deep Score"], ascending=[True,False,False]).reset_index(drop=True)
 
 # Select relevant columns for positive and negative dataframes
-sorted_posdf = sorted_df[['Match Stock', 'Label', 'lastPrice', 'mnChange', '52wLow', '52wavg','52wHigh',"Buying-Triger",'vol', 'Date', 'Deep Score']].copy()
+sorted_posdf = sorted_df[['Match Stock', 'Label', 'lastPrice', 'mnChange', '52wLow', '52wavg','52wHigh',"Diff",'Date', 'Deep Score']].copy()
 #sorted_negdf = sorted_df[['Match Stock', 'Label', 'lastPrice', 'mnChange', '52wLow', '52wavg','52wHigh', '3mAvgVol', 'vol', 'Date', 'Deep Score']].copy()
 
 # Call email functions with sorted dataframes and the original analysis dataframe
